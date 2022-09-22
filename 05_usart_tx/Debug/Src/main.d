b4d3ace27ea0d7c150442ba3e7cb79e13547ef9b
ozturkhuseyin <huse.oz99@gmail.com>
2022-09-22 03:18:33 +0300

    05_usart_tx

---
+++ 05_usart_tx/Debug/Src/main.d
@@ -0,0 +1,22 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/core_cm4.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/core_cm4.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
