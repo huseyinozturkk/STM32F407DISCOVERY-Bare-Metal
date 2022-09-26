5fe17e70b2544d49edf55962eff1613372f3af82
ozturkhuseyin <huse.oz99@gmail.com>
2022-09-26 15:06:25 +0300

    new usart projects
    added new usart projects

---
+++ 07_usart_tx_modular/Debug/Src/uart.d
@@ -0,0 +1,24 @@
+Src/uart.o: ../Src/uart.c ../Inc/uart.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/core_cm4.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+
+../Inc/uart.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/core_cm4.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/blue/Desktop/stm32/f407_bare_metal/f4-headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
